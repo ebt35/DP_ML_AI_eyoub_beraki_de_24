6f85e083b25fc33f878486a9e9a77cf13aeabfe9
ebt35 <eyoub.beraki@hotmail.com>
2025-11-25 13:19:40 +0100

    post

--- 06_fastapi_crud/api.py
+++ 06_fastapi_crud/api.py
@@ -19,5 +19,9 @@ async def read_book_by_id(id: int):
 
 
 @app.post("/books/create_book")
-async def create_book(book_requests: Book):
+async def create_book(book_request: Book):
+    new_book = Book(**book_request)
+    books.append(new_book)
+    
+    return new_book
     ...
