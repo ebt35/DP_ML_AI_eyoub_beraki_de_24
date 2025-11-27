6e14b67a71e6882947a3fd14ab05698865433353
ebt35 <eyoub.beraki@hotmail.com>
2025-11-27 10:56:06 +0100

    movies

---
+++ 07_b_pydantic_fastapi/movies_api.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from agents import movie_agent
+from data_models import Prompt
+
+app = FastAPI()
+
+
+@app.get("/movies")
+async def read_movies():
+    pass 
+
+@app.post("/movies/create_movie")
+async def create_movie(query: Prompt):
+    result = await movie_agent.run(query.prompt)
+    movie = result.output
+
+    return movie 
